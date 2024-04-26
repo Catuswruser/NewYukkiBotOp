d7af1cbd63204b8aa37c91caa92de747a985b8b0
Catuswruser <167173659+Catuswruser@users.noreply.github.com>
2024-04-26 14:54:20 +0530

    Update play.py

--- AnonXMusic/utils/inline/play.py
+++ AnonXMusic/utils/inline/play.py
@@ -52,7 +52,7 @@ def stream_markup_timer(_, chat_id, played, dur):
         bar = "————————◉—"
     else:
         bar = "🪄"
-    buttons = [
+    buttons = ]
         [
             InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
             InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
